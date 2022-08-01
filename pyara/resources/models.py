12c6b5ab148595b1956bbba3a74bc0b03d815b48
Wiola Drobik <wioleta.drobik@gmail.com>
2022-08-01 17:23:44 +0200

    feat: add User, Question and Answear models

--- pyara/resources/models.py
+++ pyara/resources/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    name = models.CharField(unique=True, max_length=100)
+
+    def __str__(self):
+        return self.email
+
+class Answear(models.Model):
+    date=models.DateField()
+    slug = models.SlugField(unique=True)
+    answear = models.TextField()
+    source = models.URLField()
+#    drawing = models.ImageField(upload_to='drawings')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.slug} - {self.author}'
+
+class Question(models.Model):
+    title = models.CharField(max_length=200)
+    date=models.DateField()
+    slug = models.SlugField(unique=True)
+    source = models.URLField()
+    answear = models.ForeignKey(Answear, blank=True, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'{self.title}'
