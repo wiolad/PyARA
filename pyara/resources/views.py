af2d8183b80dbd20ce57c5cff23fb6370181d594
Wiola Drobik <wioleta.drobik@gmail.com>
2022-08-01 15:55:12 +0200

    feat: add index.html and base.css

--- pyara/resources/views.py
+++ pyara/resources/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Welcome to PyARA - Python Amazing Resources App!')
+    return render(request, 'resources/index.html')
+#    return HttpResponse('Welcome to PyARA - Python Amazing Resources App!')
