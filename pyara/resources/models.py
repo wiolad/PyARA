146cc3fe93b61e44844bdce7b8ce931387f3dc5a
Wiola Drobik <wioleta.drobik@gmail.com>
2022-11-07 21:29:10 +0100

    feat: change Question and Answer source field from URLField to CharField, allow blank

--- pyara/resources/models.py
+++ pyara/resources/models.py
@@ -34,7 +34,7 @@ class Question(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField(default=timezone.now)
     slug = models.SlugField(unique=True)
-    source = models.URLField()
+    source = models.CharField(max_length=200, blank=True)
     author = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
@@ -44,7 +44,7 @@ class Question(models.Model):
 class Answer(models.Model):
     date = models.DateField(default=timezone.now)
     answer = models.TextField()
-    source = models.URLField()
+    source = models.CharField(max_length=200, blank=True)
 #    drawing = models.ImageField(upload_to='drawings')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, blank=True, null=True, on_delete=models.SET_NULL)
