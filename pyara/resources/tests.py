a244a6f997a155fa7b5c77a4f7097040d0e3ea7b
Wiola Drobik <wioleta.drobik@gmail.com>
2022-12-16 16:58:53 +0100

    feat: add tests for Question and Answer forms

--- pyara/resources/tests.py
+++ pyara/resources/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase, RequestFactory
 
 from .models import Question, User, Answer
+from .forms import QuestionForm, AnswerForm
 
 from django.urls import reverse
+from django.utils import timezone
 
 
 # Test Views
@@ -80,27 +82,6 @@ class UserModelTest(TestCase):
         expected_string = f'User name: {user.name}'
         self.assertEqual(str(user), expected_string)
 
-        class UserModelTest(TestCase):
-            @classmethod
-            def setUpTestData(cls):
-                number_of_users = 3
-                for user_id in range(number_of_users):
-                    User.objects.create(name=f'name_{user_id}', email=f'user_{user_id}@gmail.com')
-
-            def test_user_creation(self):
-                user = User.objects.first()
-                self.assertTrue(isinstance(user, User))
-
-            def test_user_name_max_length(self):
-                user = User.objects.get(id=1)
-                max_length = user._meta.get_field('name').max_length
-                self.assertEqual(max_length, 100)
-
-            def test_user_string_method(self):
-                user = User.objects.get(id=1)
-                expected_string = f'User name: {user.name}'
-                self.assertEqual(str(user), expected_string)
-
 
 class QuestionModelTest(TestCase):
     @classmethod
@@ -149,3 +130,43 @@ class AnswerModelTest(TestCase):
         answer = Answer.objects.get(id=1)
         expected_string = f'{answer.answer}'
         self.assertEqual(str(answer), expected_string)
+
+
+# Test Forms
+class QuestionFormTest(TestCase):
+    # Valid Form Data
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create(name='jacob', email='jacob@gmail.com')
+
+    def test_QuestionForm_valid(self):
+        form = QuestionForm(
+            data={'title': "Jaki dzisiaj dzien?", 'source': "www.strona.com", 'author': User.objects.first(),
+                  'subject': 'GEN'})
+        self.assertTrue(form.is_valid())
+
+        # Invalid Form Data
+
+    def test_QuestionForm_invalid(self):
+        form = QuestionForm(data={'title': "", 'source': "mp", 'User': User.objects.first(), 'subject': ""})
+        self.assertFalse(form.is_valid())
+
+
+class AnswerFormTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create(name='jacob', email='jacob@gmail.com')
+        Question.objects.create(title=f"question_1", slug=f"slug-1",
+                                author=User.objects.first(), subject="GEN")
+
+    def test_AnswerForm_valid(self):
+        form = AnswerForm(data={'question': Question.objects.first(), 'answer': 'Ale o co właściwie chodzi?',
+                                'source': "www.strona.com", 'date': timezone.now(), 'author': User.objects.first(),
+                                'subject': 'GEN'})
+        self.assertTrue(form.is_valid())
+
+    def test_AnswerForm_invalid(self):
+        form = AnswerForm(data={'question': Question.objects.first(), 'answer': '',
+                                'source': "www.strona.com", 'date': 'jaka data?', 'author': User.objects.first(),
+                                'subject': 'GEN'})
+        self.assertFalse(form.is_valid())
