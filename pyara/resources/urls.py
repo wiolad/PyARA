a95f53fe02571430b4a33d6684d9f27c2dedebf1
Wiola Drobik <wioleta.drobik@gmail.com>
2023-01-10 11:42:09 +0100

    chore: add name to index url

--- pyara/resources/urls.py
+++ pyara/resources/urls.py
@@ -2,10 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
+
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index-view'),
     path('questions/<question_subject>', views.questions, name='questions-list'),
     path('answer/<question_id>', views.answers, name='answers-list'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
