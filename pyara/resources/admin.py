d24a9c8859aae319cbcb5c318129864be602a709
Wiola Drobik <wioleta.drobik@gmail.com>
2023-01-11 18:06:57 +0100

    feat: change custom user model to auth model

--- pyara/resources/admin.py
+++ pyara/resources/admin.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
 
-from .models import User
 from .models import Answer
 from .models import Question
 from .models import Quote
 
+
 class ResourceAdmin(admin.ModelAdmin):
-    list_display = ('title','date')
-    list_filter = ('title','date')
-    prepopulated_fields = {'slug':('title',)}
+    list_display = ('title', 'date')
+    list_filter = ('title', 'date')
+    prepopulated_fields = {'slug': ('title',)}
+
 
 # Register your models here.
-admin.site.register(User)
 admin.site.register(Quote)
 admin.site.register(Answer)
 admin.site.register(Question, ResourceAdmin)
--- pyara/resources/models.py
+++ pyara/resources/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
+from  django.contrib.auth.models import User
 from django.urls import reverse
 
 
 # Create your models here.
-class User(models.Model):
-    email = models.EmailField(unique=True)
-    name = models.CharField(unique=True, max_length=100)
+#class User(models.Model):
+#    email = models.EmailField(unique=True)
+#    name = models.CharField(unique=True, max_length=100)
 
-    def __str__(self):
-        return f'User name: {self.name}'
+#    def __str__(self):
+#        return f'User name: {self.name}'
 
 
 class Quote(models.Model):
